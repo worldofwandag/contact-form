2c51a3aba334c6cc2ae7048c18c9ddd451fe8dfa
worldofwandag <jwandag@gmail.com>
2025-08-26 23:46:29 -0700

    Card component layout bare bones

--- app/components/Contactform.tsx
+++ app/components/Contactform.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import radioDeselect from "../assets/images/icon-radio-deselected.png";
+import checkboxEmpty from "../assets/images/icon-checkbox-empty.png";
+import Image from "next/image";
 
 const Contactform = () => {
   return (
-    <div className="card__container flex flex-col bg-white min-h-[736px] w-[736px] rounded-[16px] p-[40px] ">
-      <div className="form__content w-[656px] mx-auto">
-        <h1 className="card__title text-[32px] font-bold text-[#2A4144] tracking-[-1px] mb-[32px]">Contact Us</h1>
+    <div className="card__container flex flex-col bg-white min-h-[773px] w-[736px] rounded-[16px] p-[40px] ">
+      <div className="form__content w-[656px] mx-auto mb-[40px]">
+        <h1 className="card__title text-[32px] font-bold text-[#2A4144] tracking-[-1px] mb-[32px]">
+          Contact Us
+        </h1>
         <div className="card__fields flex flex-col ">
           {/* names */}
-          <div className="name__fields flex flex-row w-full justify-between">
+          <div className="name__fields flex flex-row w-full justify-between mb-[24px]">
             <div className="first__name w-[320px] h-[83px]">
               <div className="first__name--text mb-[8px]">First Name *</div>
               <input className="w-full " type="text" />
@@ -18,40 +23,66 @@ const Contactform = () => {
             </div>
           </div>
           {/* email */}
-          <div className="email__field">
-            <div className="email">Email Adress *</div>
-            <input type="email" />
+          <div className="email__field mb-[24px] w-full">
+            <div className="email mb-[8px]">Email Address *</div>
+            <input className="w-full" type="email" />
           </div>
           {/* query type */}
-          <div className="query__type">
-            <div className="query__title">Query Type *</div>
-            <div className="query__boxes">
-              <div className="query__general">
-                <div className="radio__status"></div>
-                <div className="general__text">General Enquiry</div>
+          <div className="query__type w-full h-[91px] mb-[24px]">
+            <div className="query__title mb-[16px]">Query Type *</div>
+
+            <div className="query__boxes flex flex-row w-full justify-between">
+              <div className="query__general w-[320px] h-[51px] query__box flex flex-row items-center">
+                <Image
+                  src={radioDeselect}
+                  alt="radio deselect"
+                  width={24}
+                  height={24}
+                  className="flex-shrink-0 mr-2"
+                />
+                <div className="general__text text-[18px] leading-[150%]">
+                  General Enquiry
+                </div>
               </div>
-              <div className="query__support">
-                <div className="radio__status"></div>
-                <div className="support__text">Support Request</div>
+              <div className="query__support w-[320px] h-[51px] query__box flex flex-row items-center">
+                <Image
+                  src={radioDeselect}
+                  alt="radio deselect"
+                  width={24}
+                  height={24}
+                  className="flex-shrink-0 mr-2"
+                />
+                <div className="support__text text-[18px] leading-[150%]">
+                  Support Request
+                </div>
               </div>
             </div>
           </div>
           {/* Message */}
-          <div className="message__field">
-            <div className="message--text">Message * </div>
-            <input className="message__box" type="text" />
+          <div className="message__field h-[137px] w-full">
+            <div className="message--text mb-[8px]">Message * </div>
+            <textarea
+              className="message__box w-full  h-[105px] text-[18px]"
+              rows={3}
+            />
           </div>
         </div>
       </div>
       {/* consent */}
-      <div className="consent__container">
-        <div className="checkbox__empty"></div>
+      <div className="consent__container flex flex-row gap-2 mb-[40px]">
+        <Image
+          src={checkboxEmpty}
+          alt="checkbox empty"
+          width={24}
+          height={24}
+          className="flex-shrink-0 mr-2"
+        />
         <div className="consent__text">
           I consent to being contacted by the team *
         </div>
       </div>
       {/* submit */}
-      <button className="submit__button w-[50px] h-[15px] rounded-[5%]">
+      <button className="submit__button w-full h-[59px] rounded-[8px] bg-[#2A4144] text-white text-[18px] font-bold leading-[150%]">
         Submit
       </button>
     </div>
